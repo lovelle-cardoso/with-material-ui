88842ff1f64d366ef26c70b6f242702b41f006d6
Lovelle Cardoso <lovelle.therese.cardoso@gmail.com>
2021-08-14 10:48:02 -0700

    Added static page with revalidate to material-ui nextjs example

---
+++ pages/about.tsx
@@ -0,0 +1,25 @@
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import * as React from 'react';
+import Copyright from '../src/Copyright';
+import Link from '../src/Link';
+import ProTip from '../src/ProTip';
+
+export default function About() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js v5-beta with TypeScript example
+        </Typography>
+        <Button variant="contained" component={Link} noLinkStyle href="/">
+          Go to the main page
+        </Button>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
---
+++ pages/static.tsx
@@ -0,0 +1,45 @@
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import { GetStaticProps } from 'next';
+import * as React from 'react';
+import Copyright from '../src/Copyright';
+import Link from '../src/Link';
+import ProTip from '../src/ProTip';
+
+interface StaticPageProps {
+  title: string;
+  buttonLabel: string;
+}
+
+export default function Static(props: StaticPageProps) {
+  const {
+title, buttonLabel
+  } = props;
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          {title}
+        </Typography>
+        <Button variant="contained" component={Link} noLinkStyle href="/">
+          {buttonLabel}
+        </Button>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
+
+
+export const getStaticProps: GetStaticProps<StaticPageProps> = async () => {
+  return {
+    props: {title: "This is a static page with revalidate", buttonLabel: "Go to the main page"},
+    // Regenerate the page:
+    // - When a request comes in
+    // - At most once every 1 second
+    revalidate: 1,
+  };
+};
